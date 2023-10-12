4c1b9a0596402e32e0c940bd3d31249e7d31f67c
Master-Victor <vvillafane@frba.utn.edu.ar>
2023-10-12 10:02:44 -0300

    DB - Se agrego la clase de conexion a la base de datos

---
+++ class/Conexion.php
@@ -0,0 +1,34 @@
+<?
+    class Conexion{
+        
+        //atributos o propiedades
+
+        public const DB_SERVER = "localhost:3463";
+        public const DB_USER = "root";
+        public const DB_PASS = "";
+        public const DB_NAME = "tiendita";
+        public const DB_DSN = "mysql:host=".DB_SERVER.";dbname=".DB_NAME.";charset=utf8mb4";
+
+        protected PDO $db;
+        //metodo contructor
+        public function __construct()
+        {
+            try{
+                $this->db = new PDO(self::DB_DSN, self::DB_USER, self::DB_PASS);
+            }catch(Exception $e){
+                die("ultimas palabras"); //exit
+            }
+        }
+        //metodo
+        
+
+        /**
+         * Función que devuelve una conexión PDO lista para usar
+         *
+         * @return PDO
+         */
+        public function getConexion(): PDO
+        {
+                return $this->db;
+        }
+    }
